762368384ab42bd19c3c4be2f5ae683cbd60b5f2
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-16 17:51:07 -0300

    Encontrando o maior

--- classe-08/index.js
+++ classe-08/index.js
@@ -1,1 +1,11 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numeros = [3, 24, 1, 8, 11, 7, 15, 39];
+
+let maiorNumero = 0;
+
+for(item of numeros){
+    if(item > maiorNumero){
+        maiorNumero = item;
+    }
+}
+
+console.log(maiorNumero);
