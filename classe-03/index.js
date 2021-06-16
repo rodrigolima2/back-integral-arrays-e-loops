57b386fcd06af078968523ff5dd0a1f3468b4598
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-16 17:15:02 -0300

    Encontre o 10

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,2 +1,12 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+let indice = -1;
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] === 10){
+        indice = i;
+        break;
+    }
+}
+
+console.log(indice);
