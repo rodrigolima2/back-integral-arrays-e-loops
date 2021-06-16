36ae30b065b008a96e2ff91c9f88a54b79c2b705
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-16 17:09:06 -0300

    Contando letras

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,1 +1,17 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+
+let contador = 0;
+
+for(item of letras){
+    if(item === "e" || item === "E"){
+        contador++;
+    }
+}
+
+if(contador == 0){
+    console.log('Nenhuma letra "E" ou "e" foi encontrada.')
+} else if(contador == 1){
+    console.log('Foi encontrada 1 letra "E" ou "e"');
+} else{
+    console.log(`Foram encontradas ${contador} letras "E" ou "e"`);
+}
