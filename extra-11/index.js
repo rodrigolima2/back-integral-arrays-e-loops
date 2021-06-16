f3dc34b3cc25e5b1a751635cc9e97f8285068590
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-16 18:13:58 -0300

    Filtrando números

--- extra-11/index.js
+++ extra-11/index.js
@@ -1,2 +1,11 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+const numEntre10e20 = [];
+
+for(item of original){
+    if(item >= 10 && item <= 20 || item > 100){
+        numEntre10e20.push(item);
+    }
+}
+
+console.log(numEntre10e20);
