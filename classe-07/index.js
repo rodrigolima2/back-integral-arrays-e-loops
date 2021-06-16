d58e0a584ea4f1b25951613d4714c1226f3d2dd9
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-16 17:47:06 -0300

    Filtrando nomes com a letra A

--- classe-07/index.js
+++ classe-07/index.js
@@ -1,1 +1,10 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomes = ["Ana", "Joana", "Carlos", "amanda", "Arlinda", "amaral", "arlequino", "jorginho", "Alok"];
+const nomesComA = [];
+
+for(item of nomes){
+    if(item[0] === "a" || item[0] === "A"){
+        nomesComA.push(item);
+    }
+}
+
+console.log(nomesComA);
